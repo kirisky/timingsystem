7d33b00be68e31f2916423291d8ba0d22c15d348
andong <andong@localhost.localdomain>
2019-09-01 19:27:47 +0000

    correct the data

--- testclient/fakedata.go
+++ testclient/fakedata.go
@@ -14,24 +14,23 @@ func getFakeData() fakedata {
 		{ Id: int32(2), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:01:00" },
 		{ Id: int32(3), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:02:00" },
 		{ Id: int32(4), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:03:00" },
-		{ Id: int32(5), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:04:00" },
-		{ Id: int32(6), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:05:00" },
-		{ Id: int32(7), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:06:00" },
-		{ Id: int32(8), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:07:00" },
-		{ Id: int32(9), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:08:00" },
-		{ Id: int32(10), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:09:00" },
-
-		// Finishi line timepoints
-		{ Id: int32(1), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:04:01" },
-		{ Id: int32(2), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:04:04" },
 		{ Id: int32(3), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:03:03" },
 		{ Id: int32(4), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:03:10" },
+		{ Id: int32(5), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:04:00" },
+		{ Id: int32(1), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:04:01" },
+		{ Id: int32(2), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:04:04" },
+		{ Id: int32(6), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:05:00" },
+		{ Id: int32(7), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:06:00" },
 		{ Id: int32(5), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:06:09" },
+		{ Id: int32(8), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:07:00" },
 		{ Id: int32(6), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:07:00" },
+		{ Id: int32(9), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:08:00" },
 		{ Id: int32(7), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:08:20" },
-		{ Id: int32(8), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:11:02" },
+		{ Id: int32(10), Type: pb.TimingSystemRequest_FINISH_CORRIDOR, TimePoint: "2019-08-08 20:09:00" },
 		{ Id: int32(9), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:09:00" },
 		{ Id: int32(10), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:10:00" },
+		{ Id: int32(8), Type: pb.TimingSystemRequest_FINISH_LINE, TimePoint: "2019-08-08 20:11:02" },
+
 	}
 
 	return data
